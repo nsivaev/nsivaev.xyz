68786c9298f8fdc5eaf2253520bdebbb166b82d4
nsivaev <nsivaev77@gmail.com>
2025-01-23 01:00:35 +0300

    fixed manifest added meta image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,8 +14,13 @@ export default defineNuxtConfig({
         { name: 'theme-color', content: '#ffffff' },
         { property: 'og:title', content: 'Мой сайт-визитка' },
         { property: 'og:description', content: 'Фронтенд-разработчик с опытом создания современных веб-приложений' },
+        { property: 'og:image', content: 'https://nsivaev.xyz/images/meta.png' },
         { property: 'og:url', content: 'https://nsivaev.xyz' },
         { property: 'og:type', content: 'website' },
+        { property: 'og:locale', content: 'ru_RU' },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' },
+        { property: 'og:image:type', content: 'image/png' }
       ]
     }
   },
