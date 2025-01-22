12b57efeebe8e2bec906ccd955b287c865b4c897
nsivaev <nsivaev77@gmail.com>
2025-01-22 12:54:54 +0300

    git init

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header/>
+    <main>
+      <h2>main</h2>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from "~/components/Header.vue";
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>Добро пожаловать на мой сайт</h1>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+header {
+  background-color: #f5f5f5;
+  text-align: center;
+  padding: 1rem;
+}
+</style>
