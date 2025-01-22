f0a0c7a8674e7bc7c2e8b5a7852a167a570097ce
nsivaev <nsivaev77@gmail.com>
2025-01-22 21:07:32 +0300

    Updates

--- _nuxt/BTqIackZ.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as o,a as n,o as c}from"./BW3EjWzf.js";const r={};function s(a,e){return c(),o("div",null,e[0]||(e[0]=[n("h1",null,"Контакты",-1),n("p",null,"Это страница контакты.",-1)]))}const _=t(r,[["render",s]]);export{_ as default};
