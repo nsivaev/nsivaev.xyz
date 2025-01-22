f03e6927a349255edef26178d3ccded9b91da6f1
nsivaev <nsivaev77@gmail.com>
2025-01-22 22:09:54 +0300

    Updates

---
+++ _nuxt/HznZ_d2l.js
@@ -0,0 +1,1 @@
+import{_ as t,c as o,a as n,o as c}from"./MJA6NmL_.js";const r={};function s(a,e){return c(),o("div",null,e[0]||(e[0]=[n("h1",null,"Контакты",-1),n("p",null,"Это страница контакты.",-1)]))}const _=t(r,[["render",s]]);export{_ as default};
