7c314f2f98e62c0ab4b77cec083ed20271b7525c
kkk <kkk@artlebedev.ru>
2025-06-02 14:04:58 +0300

    updated

--- pages/about.vue
+++ pages/about.vue
@@ -1,98 +1,26 @@
 <template>
   <div>
-    <h2 class="mb-5">Мне 22 года. Во фронтенде с конца 2022 года. С февраля 2025 — кодер-стажер в
+    <h2 class="mb-10">Родился и вырос в Волгограде. С февраля 2025 года работаю в
       <ULink
           to="https://artlebedev.ru/"
           target="_blank"
           active-class="text-primary"
           inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
-        Студии Артемия Лебедева
-      </ULink>
+        Студии Артемия Лебедева</ULink>.
     </h2>
-    <div class="mb-5">В 2023 году закончил
+    <div class="mb-3">Окончил две музыкальные школы по специальностям «скрипка» и «ударные инструменты».</div>
+    <div class="mb-3">В 2023 году закончил
       <ULink
           to="https://mvmu.mil.ru/"
           target="_blank"
           active-class="text-primary"
           inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
-        Московское Военно-музыкальное Училище
-      </ULink>
+        Московское Военно-музыкальное Училище</ULink> по классу валторны со средним профессиональным музыкальным образованием.
     </div>
-    <div class="mb-5">Курс по верстке на
-      <ULink
-          to="https://stepik.org/course/38218/promo?search=6385111185"
-          target="_blank"
-          active-class="text-primary"
-          inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
-        Stepik
-      </ULink>
-      <UContainer class="flex flex-wrap gap-3 mb-2 mt-2 px-0 sm:px-0 lg:px-0">
-        <UButton>HTML</UButton>
-        <UButton>CSS</UButton>
-        <UButton>Bootstrap</UButton>
-        <UButton>netlify</UButton>
-      </UContainer>
-    </div>
-    <div class="mb-5">Stage-0, 1 от
-      <ULink
-          to="https://rs.school/"
-          target="_blank"
-          active-class="text-primary"
-          inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
-        RS School
-      </ULink>
-      <UContainer class="flex flex-wrap gap-3 mb-2 mt-2 px-0 sm:px-0 lg:px-0">
-        <UButton>JavaScript</UButton>
-        <UButton>Git</UButton>
-        <UButton>GitHub</UButton>
-      </UContainer>
-    </div>
-    <div class="mb-5">Был опыт написания Telegram ботов
-      <ULink
-          to="https://github.com/nsivaev/randomScales/"
-          target="_blank"
-          active-class="text-primary"
-          inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
-        [репозиторий]
-      </ULink>
-    </div>
-    <div class="mb-5">С декабря 2023 года, работал в диджитал-студии
-      <UContainer class="flex flex-wrap gap-3 mb-2 mt-2 px-0 sm:px-0 lg:px-0">
-        <UButton>Laravel</UButton>
-        <UButton>PHP</UButton>
-        <UButton>CMS Wordpress</UButton>
-        <UButton>Vue 2</UButton>
-        <UButton>Nuxt 3</UButton>
-        <UButton>Astro</UButton>
-        <UButton>БЭМ</UButton>
-        <UButton>GitLab</UButton>
-      </UContainer>
-    </div>
-    <div class="mb-5">Курс по JavaScript и Python
-      <ULink
-          to="https://pll.harvard.edu/course/cs50s-web-programming-python-and-javascript"
-          target="_blank"
-          active-class="text-primary"
-          inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
-        CS50's Web Programming with Python and JavaScript
-      </ULink>
-      <UContainer class="flex flex-wrap gap-3 mb-2 mt-2 px-0 sm:px-0 lg:px-0">
-        <UButton>Python</UButton>
-        <UButton>Django</UButton>
-      </UContainer>
-    </div>
-    <div class="mb-5">С февраля 2025 года — кодер-стажер в
-      <ULink
-          to="https://artlebedev.ru"
-          target="_blank"
-          active-class="text-primary"
-          inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
-        Студии Артемия Лебедева
-      </ULink>
-      <UContainer class="flex flex-wrap gap-3 mb-2 mt-2 px-0 sm:px-0 lg:px-0">
-        <UButton>Vue 3</UButton>
-        <UButton>Nuxt 3</UButton>
-      </UContainer>
-    </div>
+    <div class="mb-3">Во фронтенде с конца 2022-го года.</div>
+    <div class="mb-3">До этого всегда любил возиться с компами и их железками. В 12 лет пробовал писать на C++. Был опыт написания тг-ботов на Python.</div>
+    <div class="mb-3">С декабря 2023 года работал в диджитал-студии.</div>
   </div>
 </template>
+<script setup lang="ts">
+</script>
